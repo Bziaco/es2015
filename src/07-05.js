83065219790192ea6f2c4164e3220e182943cd3a
bziaco <bjh9222@naver.com>
2018-04-05 17:58:06 +0900

    es2015

---
+++ src/07-05.js
@@ -0,0 +1,7 @@
+let arr = [10,20,30,40];
+let [a1,a2,a3] = arr;
+console.log(a1, a2, a3);
+
+let p1 = {name:"홍길동", age:20, gender:"M"};
+let {name, age, gender} = p1;
+console.log(name, age, gender);
